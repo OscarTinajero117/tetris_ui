21dbc683bd6ddeea70ffc1e92f2d52d0e1e37ffd
OscarTinajero117 <tinajero117.oscar@gmail.com>
2023-04-19 14:36:43 -0600

    show brick

--- lib/tetris_ui_web/live/tetris_live.ex
+++ lib/tetris_ui_web/live/tetris_live.ex
@@ -1,17 +1,20 @@
 defmodule TetrisUiWeb.TetrisLive do
   use Phoenix.LiveView
+  alias Tetris.Brick
 
   def mount(_params, _session, socket) do
     {
       :ok,
-      assign(socket, hello: "Hello", name: "Oscar")
+      assign(
+        socket, 
+        tetromino: Brick.new_random |> Brick.to_string,
+      )
     }
   end
 
   def render(assigns) do
     ~H"""
-    <h1><%= @hello %></h1>
-    <h2><%= @name %></h2>
+    <pre><%= @tetromino %></pre>
     """
   end
 end
